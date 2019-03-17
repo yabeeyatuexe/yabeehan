15093abcb3b87b29ed1861aa05e55d99c2acd832
yabeeyatuexe <rey.419seven@gmail.com>
2019-03-17 14:14:18 +0900

    botanmittuoita

--- app/src/main/java/com/example/m_062/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m_062/myapplication/MainActivity.java
@@ -10,29 +10,53 @@ import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textView1;
-    ImageView imageView;
-    int[] images = {R.drawable.manireyyyy, R.drawable.ohooooooooooooo};
-    String[] name1 = {"モ", "こ"};
+    TextView nameTextView;
+    TextView foodTextView;
+    TextView manjiTextView;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    String[] text1 = {"こ", "始", "ナイ", "クレ"};
+    String[] text2 = {"な", "皇", "チン", "オパ"};
+    String[] text3 = {"つ", "帝", "ゲール", "トラ"};
+    int[] images = {R.drawable.manireyyyy, R.drawable.ohooooooooooooo,R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        foodTextView = (TextView) findViewById(R.id.foodTextView);
+        manjiTextView = (TextView) findViewById(R.id.manjiTextView);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
 
-        textView1 = (TextView) findViewById(R.id.textView1);
-        imageView = (ImageView) findViewById(R.id.imageView);
-
-        imageView.setImageResource(R.drawable.manireyyyy);
+        imageView1.setImageResource(R.drawable.manireyyyy);
+        imageView2.setImageResource(R.drawable.manireyyyy);
+        imageView3.setImageResource(R.drawable.manireyyyy);
     }
 
-    public void button(View v) {
+    public void button1(View v) {
         Random random = new Random();
-        int index = random.nextInt(2);
-        imageView.setImageResource(images[index]);
-        textView1.setText(name1[index]);
+        int index = random.nextInt(4);
+        imageView1.setImageResource(images[index]);
+        nameTextView.setText(text1[index]);
+    }
+
+    public void button2(View v) {
+        Random random = new Random();
+        int index = random.nextInt(4);
+        imageView2.setImageResource(images[index]);
+        foodTextView.setText(text2[index]);
+    }
+
+    public void button3(View v) {
+        Random random = new Random();
+        int index = random.nextInt(4);
+        imageView3.setImageResource(images[index]);
+        manjiTextView.setText(text3[index]);
     }
 
 }
